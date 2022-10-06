5708f563bef06d5bd60b8627d956a422b8f71008
Mengzhen Liao <liaomengzhen@gmail.com>
2022-10-06 09:06:08 +0200

    add static site generation and with dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { getAllPosts, getPostById } from "../../services/postService";
+import Link from "next/link";
 
 /*
  * Make all necessary imports.
@@ -7,17 +9,37 @@ import Head from "next/head";
  * Pass down your props to the component.
  * Render the data.
  */
+export async function getStaticPaths() {
+  const posts = await getAllPosts();
+  const ids = posts.map((post) => post.id);
 
-export default function Post() {
+  return {
+    paths: ids.map((id) => ({ params: { id: id } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { id } = context.params;
+  const posts = await getPostById(id);
+
+  return {
+    props: { name: posts.name, description: posts.description, id: posts.id },
+  };
+}
+
+export default function Post({ name, description, id }) {
   return (
     <>
       <Head>
         <title>title</title>
       </Head>
-      <h1>name of the post</h1>
-      <p>ID:</p>
-      <h2>Description</h2>
-      <p></p>
+      <h1>{name} of the post</h1>
+      <p>ID: {id}</p>
+      <h2>{description}</h2>
+      <Link href="/posts" passHref>
+        Back to Posts
+      </Link>
     </>
   );
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { getAllPosts } from "../../services/postService";
+import Link from "next/link";
 
 /*
  * Make all necessary imports.
@@ -6,8 +8,15 @@ import Head from "next/head";
  * Pass down your props to the component.
  * Render the data.
  */
+export async function getStaticProps() {
+  const posts = await getAllPosts();
 
-export default function Posts() {
+  return {
+    props: { posts: posts },
+  };
+}
+
+export default function Posts({ posts }) {
   return (
     <>
       <Head>
@@ -15,7 +24,13 @@ export default function Posts() {
       </Head>
       <h1>All Posts</h1>
       <p>List of all posts</p>
-      <ul></ul>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>{post.name}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
